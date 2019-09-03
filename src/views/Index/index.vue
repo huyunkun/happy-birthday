3e25f040393e462c8595a4f4cc9e99f45a4ca0dc
earsuo <1391805182@qq.com>
2019-09-03 17:24:08 +0800

    first

---
+++ src/views/Index/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="index">
+    index
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component({})
+export default class Index extends Vue {
+  public a: any = 1
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
